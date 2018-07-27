6eb9824dfdb13c8883a48e947a383137d6b516ff
SuzanneRashka <suzanne.rashka@gmail.com>
2018-07-26 21:48:46 -0500

    working, but some bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,11 @@
-// Essentially, the app randomly picks a letter, and the user has to guess which letter the app chose. Put the following text on your page:
-// Guess what letter I'm thinking of
-// Wins: (# of times the user has guessed the letter correctly)
-// Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
-// Guesses Left: (# of guesses left. This will update)
-// Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
-// When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
-// When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
-
-
+// Guidelines
 // 1) ✔ user picks a letter
 // 2) ✔ computer picks a letter
-// 3) letters compared
-// 4) user letter stored   array.push(from user);
-// 5) repeats only 10 times.
-// 6) wins counter
-// 7) loss counter
+// 3) ✔ letters compared
+// 4) ✔ user letter stored   array.push(from user);
+// 5) ✔ repeats only 10 times.
+// 6) ✔ wins counter
+// 7) ✔ loss counter
 
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
   'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
@@ -33,13 +24,11 @@ var compLetter = " ";
 // converted number to letter.
 compLetter = alphabet[compNumber];
 
-//User Choice
 document.onkeyup = function (event) {
-
+  //User Choice holder
   var userGuess = event.key.toLowerCase();
-
-  document.getElementById("userGuess").textContent = userGuess;
-  document.getElementById("compGuess").textContent = compLetter;
+  // prints to document
+  document.getElementById("userGuess").textContent = " " + userGuess;
   document.getElementById("win").textContent = wins;
   document.getElementById("lose").textContent = losses;
   document.getElementById("remaining").textContent = guesses;
@@ -47,25 +36,32 @@ document.onkeyup = function (event) {
 
   // if user and computer guess same letter
   if (userGuess === compLetter) {
+
     wins++;
     alert("Congratulations! You've read the computers mind!");
     guesses = 10;
     guessed = [];
-    alert("press any key to continue and play another round :)");
+    alert("Press any key to continue and play another round :)"); // start game over
     compLetter;
 
+    // if 10 guesses are used
   } else if (guesses === 0) {
+
     losses++;
     guessed = [];
     guesses = 10;
-    // start game over
-    alert("You lost! Start over!");
+    alert("You lost! Start over!"); // start game over
     compLetter;
 
+    // continue game play until win or no more tries
   } else if (userGuess !== compLetter) {
+
     guesses--;
     guessed.push(userGuess);
   }
+
+  console.log("Computer Choice is " + compLetter);
+
 }
 
 //gif
@@ -83,6 +79,9 @@ toggle.addEventListener("click", function () {
 });
 
 // Problems....
+
 // 1) Command still first key press! Seriously! Stop following me!@!@!#@#!
-// 2) String.fromCharCode(event.keyCode).toLowerCase(); - not solving Meta
+// 2) Meta problem // String.fromCharCode(event.keyCode).toLowerCase(); - not solving Meta
 // 3) Computers guess only changing on refresh
+// 4) Computer choice for every user key choice
+// 5) Keystroke needed to restart board
